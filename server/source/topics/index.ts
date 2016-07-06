504c0674abbb44e81ceeb4e5465da5c89109db9b
SOHYOEN <sohyeon24@gmail.com>
2016-07-06 13:51:35 +0900

    add topic handler to subscribe a message

--- server/source/topics/index.ts
+++ server/source/topics/index.ts
@@ -2,7 +2,9 @@ import * as mqtt from 'mqtt';
 
 import hello from './hello';
 import logger from './logger';
+import subscriptionMsg from './subscriptionMsg';
 import { Things } from '../../Things';
+import { SERVER_ID } from '../../config';
 
 // Type of topic handlers
 // Topic handler takes payload, parameters, and client as input.
@@ -12,9 +14,14 @@ import { Things } from '../../Things';
 export type topicHandler = (payload, params?, client?: mqtt.Client) => void;
 
 // Add topic handlers what you want!
-export const topicHandlers: { [topic: string]: topicHandler } = {
+const topicHandlers: { [topic: string]: topicHandler } = {
     // Say hello to things...
     '+thingId/Hello': hello,
     // Just another example. Insert every messages into collection.
     '#topics': logger
 }
+
+// Server get a sub message
+topicHandlers[`${SERVER_ID}/auth/sub`] = subscriptionMsg;
+
+export { topicHandlers };
---
+++ server/source/topics/subscriptionMsg.ts
@@ -0,0 +1,5 @@
+import * as mqtt from 'mqtt';
+
+export default function subscriptionMsg(payload, params, client){
+  JSON.parse(payload)
+}
