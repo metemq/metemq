3e3071dc8e01df55c17f96b00da67c7ffeb1e802
Lim Pureum <dlavnfma22@naver.com>
2016-07-25 11:43:53 +0900

    Check the sessions when 4-way data binding

--- server/source/topics/dataBinding.ts
+++ server/source/topics/dataBinding.ts
@@ -1,9 +1,17 @@
+import { _ } from 'meteor/underscore';
 import { Source } from '../source';
 import { Things } from '../../things';
 
 export default function dataBinding(payload, params, source: Source) {
     let thingId: string = params.thingId;
     let field: string = params.field;
+
+    //Block data since thing haven't session
+    if (!_.has(source.sessions, thingId))
+        return;
+
+    if (typeof payload !== 'string' && typeof payload !== 'number')
+        return;
 
     let data = { _id: thingId };
     data[field] = payload;
