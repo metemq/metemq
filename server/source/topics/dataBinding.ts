5dc620c3f8d4aa40f02ea15420148a4ec5f41110
Jun <i2r.jun@gmail.com>
2016-08-07 17:02:44 +0900

    Fix data binding

--- server/source/topics/dataBinding.ts
+++ server/source/topics/dataBinding.ts
@@ -1,24 +1,32 @@
 import { _ } from 'meteor/underscore';
 import { Source } from '../source';
 import { Things } from '../../things';
+import { parseCSV } from '../../ddmq/util';
 
-export default function dataBinding(payload, params, source: Source) {
+export default function dataBinding(payload: string, params, source: Source) {
     let thingId: string = params.thingId;
     let field: string = params.field;
 
     //Block data since thing haven't session
-    if (!_.has(source.sessions, thingId))
-        return;
+    if (!source.hasSession(thingId)) return;
 
-    if (typeof payload !== 'string' && typeof payload !== 'number')
-        return;
+    // Check wether payload is empty
+    if (!payload.trim()) return;
 
-    let data = { _id: thingId };
-    data[field] = payload;
+    // Parse value, which can be number, string, or array of number|string
+    const value = parseValue(payload);
 
-    if (Things.find({ _id: thingId }).count() === 0) {
-        Things.insert(data);
-    } else {
-        Things.update({ _id: thingId }, data);
-    }
+    // Set update document
+    let doc = { $set: {} };
+    doc.$set[field] = value;
+
+    // Update
+    Things.update({ _id: thingId }, doc);
+}
+
+function parseValue(payload: string): number | string | Array<number | string> {
+    let value = parseCSV(payload);
+
+    if (value.length === 1) return value[0];
+    else return value;
 }
