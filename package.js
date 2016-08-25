8881105f02d5ea1e7e67ee66c2c3972e7ecaa32e
Jun <i2r.jun@gmail.com>
2016-08-25 11:25:02 +0900

    Update meteor version to 1.4.1, bump version to 0.3.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'metemq:metemq',
-  version: '0.3.1',
+  version: '0.3.2',
   // Brief, one-line summary of the package.
   summary: 'MeteMQ',
   // URL to the Git repository containing the source code for this package.
@@ -19,7 +19,7 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4');
+  api.versionsFrom('1.4.1');
   api.use('underscore@1.0.8');
   api.use('accounts-password'); // For checking password
   api.use('barbatus:typescript@0.4.0');
