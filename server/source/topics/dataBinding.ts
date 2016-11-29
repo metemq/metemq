e2a7b58074c47fd19b975383aef9f4ea280b869a
Jun Kim <i2r.jun@gmail.com>
2016-11-29 21:06:58 +0900

    Add data binding logger

--- server/source/topics/dataBinding.ts
+++ server/source/topics/dataBinding.ts
@@ -2,16 +2,25 @@ import { _ } from 'meteor/underscore';
 import { Source } from '../source';
 import { Things } from 'meteor/metemq:metemq';
 import { parseJSON } from '../../ddmq/util';
+import { getLogger } from '../../util/logger';
+
+const logger = getLogger('$bind');
 
 export default function dataBinding(payload: string, params, source: Source) {
   let thingId: string = params.thingId;
   let field: string = params.field;
 
   //Block data since thing haven't session
-  if (!source.hasSession(thingId)) return;
+  if (!source.hasSession(thingId)) {
+    logger.warn('There is no session for %s! Ignore $bind', thingId);
+    return;
+  }
 
   // Check wether payload is empty
-  if (!payload.trim()) return;
+  if (!payload.trim()) {
+    logger.warn('Payload is empty. Ignore $bind. %j', { thingId, field });
+    return;
+  }
 
   // Parse value, which can be number, string, or array of number|string
   const value = parseValue(payload);
@@ -22,6 +31,7 @@ export default function dataBinding(payload: string, params, source: Source) {
 
   // Update
   Things.update({ _id: thingId }, doc);
+  logger.info('%s updated %j', thingId, doc.$set);
 }
 
 function parseValue(payload: string): number | string | Array<number | string> {
